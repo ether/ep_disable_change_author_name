c523802f58d79144039a24f8068647a2303570b7
John McLear <john@mclear.co.uk>
2020-11-22 15:19:36 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/disable.js
+++ static/js/disable.js
@@ -3,8 +3,8 @@ var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
 exports.postAceInit = function (hook_name, args, cb) {
   $('#users input[type=text]').prop('disabled', true); // disable the text boxes in the users section.
   $('#users input[type=text]').attr('Title', 'Name Change Disabled'); // disable the text boxes in the users section.
-  $('body').append("<style> \
+  $('body').append('<style> \
     #myusernameedit{background:lightgrey !important;} \
     #myusernameedit:hover{background:lightgrey !important;color:#333 !important} \
-  </style>");
-}
+  </style>');
+};
